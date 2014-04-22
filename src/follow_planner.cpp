a3d3f761ea925963c7e9adbe1547b43230e4a38b
Paul Mathieu <paul.mathieu@pal-robotics.com>
2014-04-22 12:09:55 +0200

    follow_planner: Now it builds
    I can start hacking seriously now.

--- src/follow_planner.cpp
+++ src/follow_planner.cpp
@@ -46,102 +46,66 @@
 #include <costmap_2d/costmap_2d.h>
 
 //register this planner as a BaseFollowPlanner plugin
-PLUGINLIB_EXPORT_CLASS(follow_planner::FollowPlanner, nav_core::BaseFollowPlanner)
+PLUGINLIB_EXPORT_CLASS(follow_planner::FollowPlanner, nav_core::BaseGlobalPlanner)
 
 namespace follow_planner {
 
-FollowPlanner::FollowPlanner() :
-        costmap_(NULL), initialized_(false), allow_unknown_(true) {
-}
+FollowPlanner::FollowPlanner()
+  : initialized_(false)
+  , allow_unknown_(true)
+{}
 
-FollowPlanner::FollowPlanner(std::string name, costmap_2d::Costmap2D* costmap, std::string frame_id) :
-        costmap_(NULL), initialized_(false), allow_unknown_(true) {
+FollowPlanner::FollowPlanner(std::string name,
+                             costmap_2d::Costmap2D* costmap,
+                             std::string frame_id)
+  : initialized_(false)
+  , allow_unknown_(true)
+{
     //initialize the planner
     initialize(name, costmap, frame_id);
 }
 
-void FollowPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros) {
-    initialize(name, costmap_ros->getCostmap(), costmap_ros->getGlobalFrameID());
+void FollowPlanner::initialize(std::string name,
+                               costmap_2d::Costmap2DROS* costmap_ros)
+{
+  initialize(name, costmap_ros->getCostmap(), costmap_ros->getGlobalFrameID());
 }
 
-void FollowPlanner::initialize(std::string name, costmap_2d::Costmap2D* costmap, std::string frame_id) {
-    if (!initialized_) {
-        ros::NodeHandle private_nh("~/" + name);
-        costmap_ = costmap;
-        frame_id_ = frame_id;
+void FollowPlanner::initialize(std::string name,
+                               costmap_2d::Costmap2D* /* unused */,
+                               std::string frame_id)
+{
+  if (!initialized_)
+  {
+    ros::NodeHandle private_nh("~/" + name);
+    frame_id_ = frame_id;
 
-        unsigned int cx = costmap->getSizeInCellsX(), cy = costmap->getSizeInCellsY();
+    private_nh.param("default_tolerance", default_tolerance_, 0.0);
 
-        private_nh.param("old_navfn_behavior", old_navfn_behavior_, false);
-        if(!old_navfn_behavior_)
-            convert_offset_ = 0.5;
-        else
-            convert_offset_ = 0.0;
+    //get the tf prefix
+    ros::NodeHandle prefix_nh;
+    tf_prefix_ = tf::getPrefixParam(prefix_nh);
 
-        bool use_quadratic;
-        private_nh.param("use_quadratic", use_quadratic, true);
-        if (use_quadratic)
-            p_calc_ = new QuadraticCalculator(cx, cy);
-        else
-            p_calc_ = new PotentialCalculator(cx, cy);
-
-        bool use_dijkstra;
-        private_nh.param("use_dijkstra", use_dijkstra, true);
-        if (use_dijkstra)
-        {
-            DijkstraExpansion* de = new DijkstraExpansion(p_calc_, cx, cy);
-            if(!old_navfn_behavior_)
-                de->setPreciseStart(true);
-            planner_ = de;
-
-        }
-        else
-            planner_ = new AStarExpansion(p_calc_, cx, cy);
-
-        bool use_grid_path;
-        private_nh.param("use_grid_path", use_grid_path, false);
-        if (use_grid_path)
-            path_maker_ = new GridPath(p_calc_);
-        else
-            path_maker_ = new GradientPath(p_calc_);
-
-        plan_pub_ = private_nh.advertise<nav_msgs::Path>("plan", 1);
-        potential_pub_ = private_nh.advertise<nav_msgs::OccupancyGrid>("potential", 1);
-
-        private_nh.param("allow_unknown", allow_unknown_, true);
-        planner_->setHasUnknown(allow_unknown_);
-        private_nh.param("planner_window_x", planner_window_x_, 0.0);
-        private_nh.param("planner_window_y", planner_window_y_, 0.0);
-        private_nh.param("default_tolerance", default_tolerance_, 0.0);
-        private_nh.param("publish_scale", publish_scale_, 100);
-
-        double costmap_pub_freq;
-        private_nh.param("planner_costmap_publish_frequency", costmap_pub_freq, 0.0);
-
-        //get the tf prefix
-        ros::NodeHandle prefix_nh;
-        tf_prefix_ = tf::getPrefixParam(prefix_nh);
-
-        make_plan_srv_ = private_nh.advertiseService("make_plan", &FollowPlanner::makePlanService, this);
-
-        dsrv_ = new dynamic_reconfigure::Server<follow_planner::FollowPlannerConfig>(ros::NodeHandle("~/" + name));
-        dynamic_reconfigure::Server<follow_planner::FollowPlannerConfig>::CallbackType cb = boost::bind(
-                &FollowPlanner::reconfigureCB, this, _1, _2);
-        dsrv_->setCallback(cb);
-
-        initialized_ = true;
-    } else
-        ROS_WARN("This planner has already been initialized, you can't call it twice, doing nothing");
-
+    initialized_ = true;
+  }
+  else
+  {
+    ROS_WARN("This planner has already been initialized, you can't call it twice, doing nothing");
+  }
 }
 
-bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
-                           std::vector<geometry_msgs::PoseStamped>& plan) {
+bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                             const geometry_msgs::PoseStamped& goal,
+                             std::vector<geometry_msgs::PoseStamped>& plan)
+{
     return makePlan(start, goal, default_tolerance_, plan);
 }
 
-bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal,
-                           double tolerance, std::vector<geometry_msgs::PoseStamped>& plan) {
+bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                             const geometry_msgs::PoseStamped& goal,
+                             double tolerance,
+                             std::vector<geometry_msgs::PoseStamped>& plan)
+{
     boost::mutex::scoped_lock lock(mutex_);
     if (!initialized_) {
         ROS_ERROR(
@@ -174,56 +138,15 @@ bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geom
     unsigned int start_x_i, start_y_i, goal_x_i, goal_y_i;
     double start_x, start_y, goal_x, goal_y;
 
-    if (!costmap_->worldToMap(wx, wy, start_x_i, start_y_i)) {
-        ROS_WARN(
-                "The robot's start position is off the global costmap. Planning will always fail, are you sure the robot has been properly localized?");
-        return false;
-    }
-    if(old_navfn_behavior_){
-        start_x = start_x_i;
-        start_y = start_y_i;
-    }else{
-        worldToMap(wx, wy, start_x, start_y);
-    }
-
-    wx = goal.pose.position.x;
-    wy = goal.pose.position.y;
-
-    if (!costmap_->worldToMap(wx, wy, goal_x_i, goal_y_i)) {
-        ROS_WARN(
-                "The goal sent to the navfn planner is off the global costmap. Planning will always fail to this goal.");
-        return false;
-    }
-    if(old_navfn_behavior_){
-        goal_x = goal_x_i;
-        goal_y = goal_y_i;
-    }else{
-        worldToMap(wx, wy, goal_x, goal_y);
-    }
 
     //clear the starting cell within the costmap because we know it can't be an obstacle
     tf::Stamped<tf::Pose> start_pose;
     tf::poseStampedMsgToTF(start, start_pose);
-    clearRobotCell(start_pose, start_x_i, start_y_i);
-
-    int nx = costmap_->getSizeInCellsX(), ny = costmap_->getSizeInCellsY();
-
+    //
     //make sure to resize the underlying array that Navfn uses
-    p_calc_->setSize(nx, ny);
-    planner_->setSize(nx, ny);
-    path_maker_->setSize(nx, ny);
-    potential_array_ = new float[nx * ny];
 
-    outlineMap(costmap_->getCharMap(), nx, ny, costmap_2d::LETHAL_OBSTACLE);
-
-    bool found_legal = planner_->calculatePotentials(costmap_->getCharMap(), start_x, start_y, goal_x, goal_y,
-                                                    nx * ny * 2, potential_array_);
-
-    if(!old_navfn_behavior_)
-        planner_->clearEndpoint(costmap_->getCharMap(), potential_array_, goal_x_i, goal_y_i, 2);
-    if(publish_potential_)
-        publishPotential(potential_array_);
-
+    // TODO: change
+    bool found_legal = true;
     if (found_legal) {
         //extract the plan
         if (getPlanFromPotential(start_x, start_y, goal_x, goal_y, goal, plan)) {
@@ -240,7 +163,6 @@ bool FollowPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geom
 
     //publish the plan for visualization purposes
     publishPlan(plan);
-    delete potential_array_;
     return !plan.empty();
 }
 
